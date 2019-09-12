6f6a03c38de7a3acf320dd7559cda4abf1cc50ef
zyhou <richard.maxime9@gmail.com>
2019-09-12 14:31:24 +0200

    Add @zyhou dep

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SayHello from "@zyhou/github-action-package-demo";
 
+import logo from "./logo.svg";
+import "./App.css";
+
+SayHello();
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello, look in console</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/zyhou/github-action-package-demo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          @zyhou/github-action-package-demo
         </a>
       </header>
     </div>
